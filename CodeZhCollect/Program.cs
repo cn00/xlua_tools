2f3a11d4836a249f727e764fa7ca2424484d6dc3
cn <cool_navy@qq.com>
2019-05-09 15:37:58 +0800

    OutputPath = $(SolutionDir)/bin

---
+++ CodeZhCollect/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CodeZhCollect
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
