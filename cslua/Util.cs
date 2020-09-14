751a6e79cd37a637758aedb0af0f9f7689bf2c49
cn <cool_navy@qq.com>
2020-09-14 20:02:47 +0800

    add LinQ{Select GroupBy ToList First} for xlua

--- cslua/Util.cs
+++ cslua/Util.cs
@@ -1,11 +1,45 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using XLua;
+using RealStatePtr = System.IntPtr;
 
-namespace cslua
+namespace xlua
 {
-    public class Util
+    public static class Util
     {
+        public static void PushAny(this RealStatePtr L, object o)
+        {
+            ObjectTranslatorPool.Instance.Find(L).PushAny(L, o);
+        }
+        
+        public static List<object> ToList(IEnumerable<object> source)
+        {
+            return source.ToList();
+        }
+        public static List<IGrouping<object, object>> GroupBy(
+            IEnumerable<object> source,
+            Func<object, object> keySelector)
+        {
+            return source.GroupBy(keySelector).ToList();
+        }
+
+        public static List<object> Select(
+            IEnumerable<object> source,
+            Func<object, object> selector)
+        {
+            return source.Select(g =>  selector(g)).ToList();
+        }
+
+        public static object First(IEnumerable<object> source)
+        {
+            return source.First();
+        }
+        public static string Replace(string self, string ol, string nl)
+        {
+            return self.Replace(ol, nl);
+        }
         /*
          * public static ParallelLoopResult ForEach(IEnumerable<TSource> source, Func<TLocal> localInit, Func<TSource, ParallelLoopState, TLocal, TLocal> body, Action<TLocal> localFinally)
          */
