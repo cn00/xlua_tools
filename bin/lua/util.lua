d42f74d268de0cbaec54b83cdb5d97fb73eedd2f
cn <cool_navy@qq.com>
2020-09-27 14:08:52 +0800

    add util.{BFTrans,dump}

--- bin/lua/util.lua
+++ bin/lua/util.lua
@@ -79,10 +79,133 @@ local function OpenExcel( path )
     return wb
 end
 
+function BFTrans( strt )
+    local bf=CS.Baidu.Fanyi.Do
+    local json = require "json"
+    local t = strt
+    print("strt", t, #t)
+    local limitl = 1000
+    local batchi = 1
+    local batchl = 0
+    local batch = {}
+    local result = {}
+    for i = 1, #t do
+        local si = t[i]:gsub("\n", "嗯嗯嗯嗯嗯")
+        batchl = batchl + #si
+        batch[batchi] = si
+        batchi = batchi + 1
+        -- print("for", i, si)
+        if(batchl > limitl or i == #t)then
+            local src = table.concat( batch, "\n" )
+            local js = bf(src)
+            print("js", i, js)
+            -- {"from":"jp","to":"zh","trans_result":[{"src":"リンクスキル","dst":"链接技能"},{"src":"グループID","dst":"组ID"},{"src":"※グループIDが同じ場合高価値の大きいほうが発動","dst":"※群ID相同时，高值的一方发动"}]}	
+            local transt = json.decode(js)
+            table.insert( result, transt.trans_result)
+    
+            batchl = #si
+            batchi = 1
+            batch = {}
+        end
+    end
+    return result
+end
+
+local function dump(obj, breakline)
+    breakline = breakline == nil or breakline == true
+    local getIndent, quoteStr, wrapKey, wrapVal, dumpObj
+    getIndent = function(level)
+        if breakline then
+            return string.rep("\t", level)
+        else
+            return ""
+        end
+    end
+    quoteStr = function(str)
+        return '"' .. string.gsub(str, '"', '\"') .. '"'
+    end
+    wrapKey = function(val, breakline)
+        if breakline then
+            if type(val) == "number" then
+                return "" -- "[" .. val .. "] = "
+            elseif type(val) == "string" then
+                if string.match(val, "[][-.,%%}{)(]\"'") ~= nil then
+                    return "[" .. quoteStr(val) .. "] = "
+                else
+                    return val .. " = "
+                end
+            else
+                return "[" .. tostring(val) .. "] = "
+            end
+        else
+            if type(val) == "string" then
+                if string.match(val, "[][-.,%%}{)(]\"'") ~= nil then
+                    return quoteStr(val) .. " = "
+                else
+                    return val .. " = "
+                end
+            else
+                return ""
+            end
+        end
+    end
+    wrapVal = function(val, level, ishead)
+        if type(val) == "table" then
+            if(val.____already_dumped____)then return quoteStr("nested_table") end
+            return dumpObj(val, level, ishead)
+        elseif type(val) == "number" then
+            return val
+        elseif type(val) == "string" then
+            return quoteStr(val)
+        else
+            return tostring(val)
+        end
+    end
+    dumpObj = function(obj, level, ishead)
+        if type(obj) ~= "table" then
+            return wrapVal(obj)
+        end
+        level = level + 1
+
+        local tokens = {}
+        tokens[#tokens + 1] = "{"
+        
+        obj.____already_dumped____ = true
+        if level < 9 then
+            for k, v in pairs(obj) do
+                local head = false
+                if k == "head" then head = true end
+                if(k ~= "____already_dumped____")then
+                    local vs = getIndent(level) .. wrapKey(k) .. wrapVal(v, level, head) .. ","
+                    if type(k) == "string" and type(v) == "table" and #v > 5 then vs = vs .. " -- " .. k end
+                    tokens[#tokens + 1] = vs
+                end
+            end
+            local meta = getmetatable(obj)
+            if meta ~= nil then tokens[#tokens + 1] = getIndent(level) .. "__meta = " .. wrapVal(meta, level) .. "," end
+        else
+            tokens[#tokens + 1] = getIndent(level) .. "..."
+        end
+        tokens[#tokens + 1] = getIndent(level - 1) .. "}"
+        if breakline then
+            if #tokens < 6 or ishead then
+                local st = table.concat(tokens, " "):gsub("%s%s+", " ")
+                if #st < 50 or ishead  then return st end
+            end
+            return table.concat(tokens, "\n")
+        else
+            return table.concat(tokens, " ")
+        end
+    end
+    return dumpObj(obj, 0)
+end
+
 return {
     OpenExcel = OpenExcel,
     SaveWorkbook = SaveWorkbook,
     loadNPIO = loadNPIO,
 	JpMatch = JpMatch,
-	GetFiles = GetFiles
+    GetFiles = GetFiles,
+    BFTrans = BFTrans,
+    dump = dump
 }
